fa23b19e7e37957e107cb0fc835902d6b389dcad
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-22 17:33:47 -0300

    Update -test1.js

--- plugins/-test1.js
+++ plugins/-test1.js
@@ -2,25 +2,35 @@ let esperaRespuesta = {};
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
   if (command === 'prueba') {
-    esperaRespuesta[m.sender.id] = true;
-    let pruebaXd = `Mensaje de Prueba. Responda a este mensaje con Si para confirmar. Responda a este mensaje con No para denegar.`;
-    await conn.sendMessage(m.chat, { text: pruebaXd }, { quoted: m });
+    try {
+      esperaRespuesta[m.sender.id] = true;
+      let pruebaXd = `Mensaje de Prueba. Responda a este mensaje con Si para confirmar. Responda a este mensaje con No para denegar.`;
+      await conn.sendMessage(m.chat, { text: pruebaXd }, { quoted: m });
+      console.log('Mensaje enviado correctamente');
+    } catch (error) {
+      console.log('Error al enviar mensaje:', error);
+    }
   }
 };
 
 handler.before = async (m, { conn }) => {
-  const text = m.text.trim().toLowerCase();
-  if (esperaRespuesta[m.sender.id]) {
-    if (text === 'si') {
-      await conn.sendMessage(m.chat, { text: 'Confirmado!' }, { quoted: m });
-      delete esperaRespuesta[m.sender.id];
-    } else if (text === 'no') {
-      await conn.sendMessage(m.chat, { text: 'Denegado!' }, { quoted: m });
-      delete esperaRespuesta[m.sender.id];
-    } else {
-      await conn.sendMessage(m.chat, { text: 'Opción inválida. Por favor, responde con Si o No.' }, { quoted: m });
+  try {
+    const text = m.text.trim().toLowerCase();
+    if (esperaRespuesta[m.sender.id]) {
+      if (text === 'si') {
+        await conn.sendMessage(m.chat, { text: 'Confirmado!' }, { quoted: m });
+        delete esperaRespuesta[m.sender.id];
+      } else if (text === 'no') {
+        await conn.sendMessage(m.chat, { text: 'Denegado!' }, { quoted: m });
+        delete esperaRespuesta[m.sender.id];
+      } else {
+        await conn.sendMessage(m.chat, { text: 'Opción inválida. Por favor, responde con Si o No.' }, { quoted: m });
+      }
+      return true;
     }
-    return true;
+  } catch (error) {
+    console.log('Error en before:', error);
+    await conn.reply(m.chat, `${error}`, m)
   }
   return true;
 };
