61a72045bf0a82ddd983ec6029c27357d43dee80
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-22 17:40:59 -0300

    Update -test1.js

--- plugins/-test1.js
+++ plugins/-test1.js
@@ -1,17 +1,12 @@
 let esperaRespuesta = {};
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
-  console.log('Comando recibido:', command);
-  console.log('ID del remitente:', m.sender.id);
-  console.log('ID del chat:', m.chat.id);
-
   if (command === 'prueba') {
     try {
-      esperaRespuesta[m.sender.id] = true;
+      const id = `${m.chat.id}_${m.sender.id}`;
+      esperaRespuesta[id] = true;
       let pruebaXd = `Mensaje de Prueba. Responda a este mensaje con Si para confirmar. Responda a este mensaje con No para denegar.`;
-      console.log('Enviando mensaje...');
-      let enviado = await conn.sendMessage(m.chat.id, { text: pruebaXd }, { quoted: m });
-      console.log('Mensaje enviado:', enviado);
+      await conn.sendMessage(m.chat.id, { text: pruebaXd }, { quoted: m });
     } catch (error) {
       console.log('Error al enviar mensaje:', error);
     }
@@ -20,17 +15,15 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
 
 handler.before = async (m, { conn }) => {
   try {
-    console.log('Before ejecutado');
-    const text = m.text.trim().toLowerCase();
-    console.log('Texto recibido:', text);
-    if (esperaRespuesta[m.sender.id]) {
-      console.log('Respuesta esperada');
+    const id = `${m.chat.id}_${m.sender.id}`;
+    if (esperaRespuesta[id]) {
+      const text = m.text.trim().toLowerCase();
       if (text === 'si') {
         await conn.sendMessage(m.chat.id, { text: 'Confirmado!' }, { quoted: m });
-        delete esperaRespuesta[m.sender.id];
+        delete esperaRespuesta[id];
       } else if (text === 'no') {
         await conn.sendMessage(m.chat.id, { text: 'Denegado!' }, { quoted: m });
-        delete esperaRespuesta[m.sender.id];
+        delete esperaRespuesta[id];
       } else {
         await conn.sendMessage(m.chat.id, { text: 'Opción inválida. Por favor, responde con Si o No.' }, { quoted: m });
       }
