58225ebca21e989ed4170e0c538db73d431ec540
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-22 17:29:44 -0300

    Update -test1.js

--- plugins/-test1.js
+++ plugins/-test1.js
@@ -2,24 +2,27 @@ let esperaRespuesta = {};
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
   if (command === 'prueba') {
-    let id = m.sender.id + m.chat.id;
-    esperaRespuesta[id] = { esperando: true, mensaje: m };
+    esperaRespuesta[m.sender.id] = true;
     let pruebaXd = `Mensaje de Prueba. Responda a este mensaje con Si para confirmar. Responda a este mensaje con No para denegar.`;
     await conn.sendMessage(m.chat, { text: pruebaXd }, { quoted: m });
-  } else {
-    let id = m.sender.id + m.chat.id;
-    if (esperaRespuesta[id] && esperaRespuesta[id].esperando) {
-      if (m.text.toLowerCase() === 'si') {
-        await conn.sendMessage(m.chat, { text: 'Confirmado!' }, { quoted: m });
-      } else if (m.text.toLowerCase() === 'no') {
-        await conn.sendMessage(m.chat, { text: 'Denegado!' }, { quoted: m });
-      } else {
-        await conn.sendMessage(m.chat, { text: 'Opción inválida. Por favor, responde con Si o No.' }, { quoted: m });
-        return;
-      }
-      delete esperaRespuesta[id];
-    }
   }
+};
+
+handler.before = async (m, { conn }) => {
+  const text = m.text.trim().toLowerCase();
+  if (esperaRespuesta[m.sender.id]) {
+    if (text === 'si') {
+      await conn.sendMessage(m.chat, { text: 'Confirmado!' }, { quoted: m });
+      delete esperaRespuesta[m.sender.id];
+    } else if (text === 'no') {
+      await conn.sendMessage(m.chat, { text: 'Denegado!' }, { quoted: m });
+      delete esperaRespuesta[m.sender.id];
+    } else {
+      await conn.sendMessage(m.chat, { text: 'Opción inválida. Por favor, responde con Si o No.' }, { quoted: m });
+    }
+    return true;
+  }
+  return true;
 };
 
 handler.command = ['prueba'];
