85b3b9b91119d5a2f5a15212c0b6961dcbc459a7
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-22 18:42:48 -0300

    Update -test1.js

--- plugins/-test1.js
+++ plugins/-test1.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime.startsWith('image/')) return conn.sendMessage(m.chat, { text: `Ingrese el comando y responda a una imagen para describirla.`}, { quoted: m })
+if (!mime.startsWith('image/')) return conn.sendMessage(m.chat, { text: `Ingrese el comando y responda a una imagen semi vieja para revivir sus colores.` }, { quoted: m })
 await m.react('⏳')
 let media = await q.download()
 let formData = new FormData()
@@ -16,12 +16,12 @@ headers: {
 ...formData.getHeaders()
 }})
 let imgs = upload.data.data.url
-let response = await fetch(`https://apis-starlights-team.koyeb.app/starlight/describe-picture?url=${imgs}&lang=es`) 
-let json = await response.json()
-await conn.sendMessage(m.chat, { text: json.data.desc }, { quoted: m })
-//conn.sendFile(m.chat, imgs, 'image.jpg', json.data.desc, m)
+let colors = await fetch(`https://apis-starlights-team.koyeb.app/starlight/colorize-ai?url=${imgs)}`)
+let json = await colors.json()
+await conn.sendMessage(m.chat, { image: { url: json.url }, caption: `${wm}` }, { quoted: m })
+//conn.sendFile(m.chat, json.url, 'colorized.jpg', null, m)
 }
-handler.command = ["describe", "e-desc"]
+handler.command = ["e-rize", "rize"]
 export default handler
 
   
