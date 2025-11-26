1344360bce5e1dfe0d95adee5ae9493489a19cf4
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-26 08:58:29 -0300

    Update hr-lid.js

--- plugins/hr-lid.js
+++ plugins/hr-lid.js
@@ -4,9 +4,9 @@ const mentionedJid = await m.mentionedJid
 const userId = mentionedJid.length > 0 ? mentionedJid[0] : (m.quoted ? await m.quoted.sender : m.sender)
 const participant = participantList.find(participant => participant.id === userId)
 if (participant) {
-await conn.sendMessage(m.chat, { text: `📁  *LID:* @${userId.split('@')[0]} = CODE ${participant.lid}`, mentions: [userId] }, { quoted: m })
+await conn.sendMessage(m.chat, { text: `🜲 Usuario : @${userId.split('@')[0]}\n⚶ Lid : ${participant.lid}`, mentions: [userId] }, { quoted: m })
 } else {
-await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = Command error, try again and if the error persists, report the command.` }, { quoted: m })
+await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = error_lid` }, { quoted: m })
 }}
 
 handler.command = ['lid', 'mylid']
