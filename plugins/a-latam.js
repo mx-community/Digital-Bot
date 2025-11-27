f895596f2f096e3e136524794e71c84b99e2c5a7
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-27 08:03:11 -0300

    Update a-latam.js

--- plugins/a-latam.js
+++ plugins/a-latam.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin, isOwner }) => {
 let chat = global.db.data.chats[m.chat]
 
-if (command === 'c-latam') {
+if (command === 'f-latam') {
 if (!m.isGroup) return conn.reply(m.chat, `📍  Solo puedes aplicar esta función en chats grupales.`, m)
 if (!isAdmin && !isOwner) return conn.reply(m.chat, `📍  Solo los administradores pueden activar esta función.`, m)
 if (!isBotAdmin) return conn.reply(m.chat, `📍  Solo funciona si el bot es administrador.\n- No aplica nada si la función solo es para administración.`, m)
@@ -9,14 +9,14 @@ if (!isBotAdmin) return conn.reply(m.chat, `📍  Solo funciona si el bot es adm
 let action = args[0]?.toLowerCase()
 
 if (action === 'on') {
-chat.fLatinos = true
+chat.fLatam = true
 conn.reply(m.chat, `✅ \`\`\`Funcion activado con exito.\`\`\``, m)
 } else if (action === 'off') {
-chat.fLatinos = false
+chat.fLatam = false
 conn.reply(m.chat, `✅ \`\`\`Funcion desactivado con exito.\`\`\``, m)
 
 } else {
-let status = chat.fLatinos ? 'activo' : 'desactivado'
+let status = chat.fLatam ? 'activo' : 'desactivado'
 conn.reply(m.chat, `📍  La funcion ya esta ${status}.`, m)
 }
 }
@@ -26,7 +26,7 @@ handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner }) {
 if (!m.isGroup) return 
 
 let chat = global.db.data.chats[m.chat]
-if (!chat.onlyLatinos) return
+if (!chat.fLatam) return
 
 if (isBotAdmin && !isAdmin && !isOwner) {
 let badPrefixes = ["212", "234", "92", "93", "967"]
