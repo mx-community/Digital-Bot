8f906ebe403c897012e19c35ef22f5c3c18f76c2
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-29 07:59:31 -0300

    Update -test2.js

--- plugins/-test2.js
+++ plugins/-test2.js
@@ -1,7 +1,8 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-let love = `❪ ✎ › El amor de *@${name}* hacia a ti es un *${Math.floor(Math.random() * 100)}%*`
+if (!who) return conn.reply(m.chat, `mensione`, m)
+  let love = `❪ ✎ › El amor de *@${who.split(`@`)[0]}* hacia a ti es un *${Math.floor(Math.random() * 100)}%*`
 conn.sendMessage(m.chat, {text: love, mentions: [who]}, {quoted: m})}
 handler.command = ["love"]
 export default handler
