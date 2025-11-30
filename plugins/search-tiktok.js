807a438fd4aad799114aac7833c7db25a0dbaeca
MX-DIGITAL <emprendejosuev@gmail.com>
2025-11-30 05:02:05 -0300

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -1,10 +1,24 @@
-/*import axios from 'axios'
+import axios from 'axios'
 
 const handler = async (m, { conn, text, usedPrefix, command}) => {
-if (!text) return conn.sendMessage(m.chat, { text: `Ingrese el comando y escriba lo que quiere buscar en TikTok.\n\n• Por ejemplo:\n*${usedPrefix + command}* Trends de baile.` }, { quoted: m })
+let opcionesXd = `\t〤  *T I K T O K  :  D L*
+
+\t\t⸭ 📌 \`\`\`Descargas de tiktok.\`\`\`
+
+\t\t⧡ .tiktok : *<link>*
+\t\t⧡ .tiktoks : *<text>*
+\t\t⧡ .p-tiktok : *<link>*
+\t\t⧡ .a-tiktok : *<link>*
+
+⚶ Por ejemplo:
+*${usedPrefix + command} trends de baile
+
+> ${textbot}`
+if (!text) return conn.sendMessage(m.chat, { text: opcionesXd }, { quoted: m })
 const isUrl = /(?:https:?\/{2})?(?:www\.|vm\.|vt\.|t\.)?tiktok\.com\/([^\s&]+)/gi.test(text)
 try {
-await conn.sendMessage(m.chat, { text: `Buscando resultados, espere un momento...` }, { quoted: m })
+await m.react('⏰')
+  //conn.sendMessage(m.chat, { text: `Buscando resultados, espere un momento...` }, { quoted: m })
 if (isUrl) {
 const res = await axios.get(`https://www.tikwm.com/api/?url=${encodeURIComponent(text)}?hd=1`)
 const data = res.data?.data;
@@ -21,13 +35,13 @@ await conn.sendMessage(m.chat, { video: { url: play }, caption }, { quoted: m })
 }} else {
 const res = await axios({ method: 'POST', url: 'https://tikwm.com/api/feed/search', headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Cookie': 'current_language=en', 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36' }, data: { keywords: text, count: 20, cursor: 0, HD: 1 }})
 const results = res.data?.data?.videos?.filter(v => v.play) || []
-if (results.length < 2) return conn.reply(m.chat, 'ꕥ Se requieren al menos 2 resultados válidos con contenido.', m)
+if (results.length < 2) return conn.reply(m.chat, '📍 Se requieren al menos 2 resultados válidos con contenido.', m)
 const medias = results.slice(0, 10).map(v => ({ type: 'video', data: { url: v.play }, caption: createSearchCaption(v) }))
 await conn.sendSylphy(m.chat, medias, { quoted: fkontak })
 }
 await m.react('✅')
 } catch (e) {
-await m.react('📍')
+await m.react('❌')
 await await conn.sendMessage(m.chat, { text: `*[ 📍 ]*  ERROR_COMMAND = ${e}` }, { quoted: m })
 }}
 function createCaption(title, author, duration, created_at = '') {
@@ -43,7 +57,7 @@ handler.command = ['tiktoks', 'tts']
 
 
 export default handler
-  */
+  
 
 /*import axios from "axios";
 const { proto, generateWAMessageFromContent, generateWAMessageContent, } = (await import("@whiskeysockets/baileys")).default;
